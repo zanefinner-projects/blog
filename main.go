a46f690daab8f3332d3e6ef444355e76a1c9d09c
Zane Finner <zanefinnergm@gmail.com>
2020-05-28 16:21:53 -0400

    init templating structure

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/zanefinner-templates/blog/base"
+)
 
 func main() {
 	//Handlers
 	http.HandleFunc("/", base.Index)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
---
+++ templates/structure.go
@@ -0,0 +1,9 @@
+package templates
+
+//BlogPostPage is a structure for templating the Blog Posts
+type BlogPostPage struct {
+	Title    string
+	Body     string
+	Author   string
+	PostedAt string
+}
