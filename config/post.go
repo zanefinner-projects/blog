0ce05483e85dc3a4c545567d12a642be2126ebd6
Zane Finner <zanefinnergm@gmail.com>
2020-06-01 16:53:24 -0400

    create posts requires authentication

---
+++ config/post.go
@@ -0,0 +1,6 @@
+package config
+
+//GetPostKey returns a key for posting articles
+func GetPostKey() string {
+	return "a4dfsb"
+}
--- posts/create.go
+++ posts/create.go
@@ -17,27 +17,30 @@ func Create(w http.ResponseWriter, r *http.Request) {
 
 	} else if r.Method == http.MethodPost {
 		fmt.Println("POST: /posts/create")
-		r.ParseForm()
-		success, message := validate(r.PostFormValue("title"), r.PostFormValue("content"))
-		if success {
-			name, pword, dbname := config.GetDbCreds()
-			db, err := sql.Open("mysql", name+":"+pword+"@/"+dbname)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			var insForm *sql.Stmt
-			var res sql.Result
-			insForm, err = db.Prepare("INSERT INTO posts(title, content) VALUES(?,?)")
-			res, err = insForm.Exec(r.PostFormValue("title"), r.PostFormValue("content"))
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(res)
+		loggedIn := matchAuth(r.PostFormValue("email"), r.PostFormValue("password"), r.PostFormValue("key"))
+		if loggedIn {
+			r.ParseForm()
+			success, message := validate(r.PostFormValue("title"), r.PostFormValue("content"))
+			if success {
+				name, pword, dbname := config.GetDbCreds()
+				db, err := sql.Open("mysql", name+":"+pword+"@/"+dbname)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				var insForm *sql.Stmt
+				var res sql.Result
+				insForm, err = db.Prepare("INSERT INTO posts(title, content) VALUES(?,?)")
+				res, err = insForm.Exec(r.PostFormValue("title"), r.PostFormValue("content"))
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				fmt.Println(res)
 
-		} else {
-			fmt.Println(message)
+			} else {
+				fmt.Println(message)
+			}
 		}
 	}
 }
@@ -47,4 +50,21 @@ func validate(title, content string) (bool, string) {
 		return false, "Title must be 8-128 chars, Content must be 17-3400 chars"
 	}
 	return true, ""
+}
+
+func matchAuth(email, password, key string) bool {
+	id := 0
+	name, pword, dbname := config.GetDbCreds()
+	db, err := sql.Open("mysql", name+":"+pword+"@/"+dbname)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	if key == config.GetPostKey() {
+		err = db.QueryRow("SELECT id FROM accounts where email = ? and password = ?", email, password).Scan(&id)
+		if id != 0 {
+			return true
+		}
+	}
+	return false
 }
