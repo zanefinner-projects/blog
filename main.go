d8a7af0b48c8ddcc1cac841cfdd61aacf15add3b
Zane Finner <zanefinnergm@gmail.com>
2020-06-14 18:31:01 -0400

    --

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	//Handlers
 	///Base
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {git p
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
 			fmt.Fprint(w, "Not found :/")
 			return
